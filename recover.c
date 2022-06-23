37d482c51b10ef556c2376084508f48e9b044e09
M.Yavuz Yagis <mehmetyavuzyagis@gmail.com>
2022-06-23 18:22:26 +0300

    refactor

--- recover.c
+++ recover.c
@@ -3,19 +3,11 @@
 #include<stdlib.h>
 #include <unistd.h>
 
-
-// the program should only take one argument, which is the name of the forensic image!
-// faulty usage must return 1 and urge the user
-// if forensic image cannot be read, the user must be prompted so.
-// names of the rescued files should be consecutive triple digit integer names - 000.jpg, 001.jpg etc, 
 // is use malloc, no memory leak should happen!
 
 int main(int argc, char *argv[])
 {
  
-
- // lets check the argcs
-
  if(argc !=2){
      fprintf(stderr,"Program takes only one parameter which is the forensic image.\n");
      return EXIT_FAILURE;
@@ -27,9 +19,6 @@ if(forensic==NULL){
     return EXIT_FAILURE;
 }
 
-
-
-
 // lets define some generic values to use iteratively
 
 int blocksize = 512;
@@ -38,8 +27,6 @@ int counter = 0; // number of founded items
 char name[8]; // 000.jpg + \0
 unsigned char* buff;
 buff= malloc(blocksize); // This is the 512 byte buff are to read and write the chunks.
-
-// took help from here : https://stackoverflow.com/questions/38302220/error-when-comparing-array-elements-to-constant-c
 
 while(fread(buff, 512, 1 ,forensic)){     
     // forensici oku, 512 btylar seklinde, ve buffa yaz.
@@ -51,17 +38,12 @@ while(fread(buff, 512, 1 ,forensic)){
             fclose(image);
         }
 
-        // now we have some buffs to write but this file is not opened and does not have a name
-
         sprintf(name, "%03d.jpg", counter);  // name will be contigous,
         printf("name for the image %s is being created automatically\n",name);
         image = fopen(name,"w");  // open the image with the name in the write mode
         printf("opening image %s\n",name);
         printf("writing info into the image %s\n",name);
         printf("image %s is created succesfully\n",name);
-
-
-        // but what if image is empty? i.e. could not allocate memory?
 
         if(image== NULL){
             fclose(forensic);
@@ -74,23 +56,11 @@ while(fread(buff, 512, 1 ,forensic)){
 
     }
 
-    // so we have found an image, created another empty image and counter is 1. we need to write it.
-
     if(counter>0){
         
         fwrite(buff, 512,1,image);
     }
-
-
-
-
 }
-
-
-
-
-
-
 
 fclose(image);
 fclose(forensic);
